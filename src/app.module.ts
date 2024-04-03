74deb0de62d526b705e3475dcb893280cd65571c
DESKTOP-RMH1MSE\calde <gaston.caldera.developer@gmail.com>
2024-04-03 16:01:48 -0400

    Change on gitignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,10 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.local', '.env'],
+    }),
     MongooseModule.forRoot(
       process.env.DATABASE_HOST
         ? process.env.DATABASE_HOST
